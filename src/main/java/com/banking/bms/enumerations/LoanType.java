f2d6562e3ad1f4c6a3d6db26c6be7ebb21342a13
KirtanBhavsar2193 <kirtanbhavsar21@gmail.com>
2025-07-18 17:49:37 +0530

    create Loan entity

---
+++ src/main/java/com/banking/bms/enumerations/LoanType.java
@@ -0,0 +1,28 @@
+package com.banking.bms.enumerations;
+
+public enum LoanType {
+
+    // Secured Loans
+    HOME_LOAN,
+    CAR_LOAN,
+    GOLD_LOAN,
+    LOAN_AGAINST_PROPERTY,
+    SECURED_BUSINESS_LOAN,
+
+    // Unsecured Loans
+    PERSONAL_LOAN,
+    EDUCATION_LOAN,
+    UNSECURED_BUSINESS_LOAN,
+    CREDIT_CARD_LOAN,
+
+    // Special Purpose Loans
+    AGRICULTURAL_LOAN,
+    HOME_IMPROVEMENT_LOAN,
+    CONSUMER_DURABLE_LOAN,
+    STARTUP_LOAN,
+
+    // Other Types
+    PAYDAY_LOAN,
+    MICROFINANCE_LOAN,
+    OVERDRAFT_LOAN
+}
---
+++ src/main/java/com/banking/bms/model/entities/Loan.java
@@ -0,0 +1,85 @@
+package com.banking.bms.model.entities;
+
+import com.banking.bms.enumerations.LoanStatus;
+import com.banking.bms.enumerations.LoanType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+@Entity
+@Table(name = "loan")
+@Data
+public class Loan {
+
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "uuid2")
+    @Column(name = "loan_id")
+    private String loanId;
+
+    @Column(name = "loan_number", unique = true)
+    private Long loanNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "loan_type")
+    private LoanType loanType;
+
+    @Column(name = "loan_amount")
+    private double loanAmount;
+
+    @Column(name = "interest_rate")
+    private double interestRate;
+
+    @Column(name = "loan_term")
+    private Integer loanTerm;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "loan_status")
+    private LoanStatus loanStatus;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customerId;
+
+    @ManyToOne
+    @JoinColumn(name = "approved_by")
+    private User approvedBy;
+
+    @Column(name = "remarks")
+    private String remarks;
+
+    @Column(name = "emi_paid_count")
+    private Integer emiPaidCount = 0;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.loanNumber = generateLoanNumber();
+    }
+
+    private Long generateLoanNumber() {
+        Random random = new Random();
+        long min = 1000L;
+        long max = 9999L;
+        return min + ((long) (random.nextDouble() * (max - min)));
+    }
+}
